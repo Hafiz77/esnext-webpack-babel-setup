1f08fec71809a5441edf0c04f8464ce494433ad1
Nur Rony <pro.nmrony@gmail.com>
2015-11-14 12:18:05 +0600

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/app',
+    'webpack-dev-server/client?http://localhost:8080'
+  ],
+  output: {
+      publicPath: '/build',
+      filename: 'app.js'
+  },
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./src",
+    hot: true
+  }
+};
